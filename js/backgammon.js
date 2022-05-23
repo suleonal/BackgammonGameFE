683a9d4b565b2364489bf84831d5311fc874e319
suleonal <suleonal8@gmail.com>
2022-05-23 16:10:00 +0300

    panel yapimi

--- js/backgammon.js
+++ js/backgammon.js
@@ -1,10 +1,14 @@
-URL_backgammon_API = "http://localhost:8080/backgammon/start"
+URL_backgammon_API = "http://localhost:8080/backgammon"
 
 
 backgammonGameInfo = {
     sessionId: "",
-    backgammonBoard: {}
+    backgammonBoard: {},
+    moveSrc: 51,
+    moveDest: 51
 }
+
+magicNumber = 51;
 
 function startGame() {
     $.get(URL_backgammon_API + "/start", function (result) {
@@ -21,14 +25,29 @@ function getBoard() {
     });
 }
 
-function sendPlayerMove(pitId) {
-    request = $.get(URL_backgammon_API + "/move/" + backgammonGameInfo.sessionId + "/" + pitId);
+
+function rollDice() {
+    $.get(URL_backgammon_API + "/roll/" + backgammonGameInfo.sessionId, function (result) {
+        backgammonGameInfo.backgammonBoard = result.backgammonBoard;
+        drawBoard(backgammonGameInfo.backgammonBoard);
+    });
+}
+
+function sendPlayerMove() {
+    request = $.get(URL_backgammon_API + "/move/" + backgammonGameInfo.sessionId + "/" + backgammonGameInfo.moveSrc + "/" +backgammonGameInfo.moveDest);
     request.done(function (result) {
+        backgammonGameInfo.moveSrc = magicNumber;
+        backgammonGameInfo.moveDest = magicNumber;
+        if(result.backgammonBoard.currentPlayer != backgammonGameInfo.backgammonBoard.currentPlayer){
+            $(".diceData").html("<li>Roll Dice</li>");
+        }
         backgammonGameInfo.backgammonBoard = result.backgammonBoard;
         drawBoard(backgammonGameInfo.backgammonBoard);
     });
     request.fail(function (xhr, statusText, errorThrown) {
         alert(JSON.parse(xhr.responseText).message);
+        backgammonGameInfo.moveSrc = magicNumber;
+        backgammonGameInfo.moveDest = magicNumber;
     });
 }
 
@@ -46,16 +65,38 @@ function completeGame(backgammonBoard) {
 
 function drawBoard(backgammonBoard) {
     $(".playerName").html("Player " + backgammonBoard.currentPlayer);
-    for (const player in backgammonBoard.boards) {
-        pits = backgammonBoard.boards[player].pits;
-        for (const pit in pits) {
-            $("#player" + player + "Pit" + pit).html(pits[pit]);
+
+    pits = backgammonBoard.pits;
+    for (const pit in pits) {
+        pitData = pits[pit];
+        listData = "";
+        for (const stoneId in pitData) {
+            stone = pitData[stoneId];
+            stoneType = stone.player == 'ONE' ? './img/white-stamp.png' : './img/black-stamp.png';
+            listData = listData + '<li><a href="#" class="stoneButton" id="stoneButton_' + pit + '" ><img src="' + stoneType + '"/></a></li>';
         }
-        $("#player" + player + "Treasure").html(backgammonBoard.boards[player].treasure);
+        if (listData === "") {
+            listData = "<li><a href='#' class='stoneButton' id='stoneButton_" + pit + "' ><span>" + pit + "</span></a></li>"
+        }
+        $("#" + pit).html(listData);
+
     }
+
+    if (backgammonBoard.moves.length > 0) {
+        dicesData = "";
+        for (const moveId in backgammonBoard.moves) {
+            dice = backgammonBoard.moves[moveId];
+            dicesData = dicesData + "<li>" + dice + "</li>";
+        }
+        $(".diceData").html(dicesData);
+    }
+
+
     if (backgammonBoard.gameState === 'COMPLETED') {
         completeGame(backgammonBoard);
     }
+
+    bindStoneButtons();
 }
 
 function bindButtons() {
@@ -63,52 +104,64 @@ function bindButtons() {
         startGame();
     });
 
-    $(".pitButton").click(function () {
-        id = $(this).find(".pitText").attr("id");
-        pitId = id.substr(-1);
-        playerName = id.substr(6, 3);
-        if (backgammonGameInfo.backgammonBoard.currentPlayer != playerName) {
-            alert("It is not your turn");
-            return;
+    
+    $(".diceButton").click(function () {
+        rollDice();
+    });
+    bindStoneButtons();
+
+}
+
+function bindStoneButtons() {
+    $(".stoneButton").click(function () {
+        if (backgammonGameInfo.moveSrc == magicNumber) {
+            backgammonGameInfo.moveSrc = $(this).attr("id").substr(12);
+        } else {
+            backgammonGameInfo.moveDest = $(this).attr("id").substr(12);
         }
-        sendPlayerMove(pitId);
+        if (backgammonGameInfo.moveSrc != magicNumber && backgammonGameInfo.moveDest != magicNumber) {
+            sendPlayerMove();
+        }
     });
 }
+
+
 
 function setBoardSize() {
     ratio = $(".board").width() / 358;
     $(".board").height(ratio * 366);
-    $(".board table tbody").height(ratio*366);
-  
+    $(".board table tbody").height(ratio * 366);
 }
 
 function createBoardButtons() {
     pitSize = 12;
-    players = ['ONE', 'TWO']
-    $.each(players, function (index, player) {
+    boardRows = [0, 1, 2]
+    $.each(boardRows, function (index, boardRow) {
         rowData = "<tr>";
-        treasure = '<td rowspan=2 class="treasure">' +
-                '<a class="button " href="#" >' +
-                '<span id="player{PLAYERNAME}Treasure" class="text" >1</span>' +
-                '</a>' +
-                '</td>';
-        for (pit = 0; pit < pitSize; pit++) {
-            // if (player === 'ONE' && pit === 0) {
-            //     rowData = rowData + treasure.replace("{PLAYERNAME}", "ONE");
-            // }
-            rowData = rowData + '<td class="pit">' +
-                    '<a class="button pitButton" href="#" >' +
-                    '<span id="player' + player + 'Pit' + pit + '" class="text pitText">1</span>' +
-                    '</a>' +
-                    '</td>';
+        if (boardRow == 1) {
 
-            if (pit === 5) {
-                rowData = rowData + '<td class="boardCenter ">&nbsp;</td>';
+            rowData = "<td id='Dices' colspan='13'><a href='#' class='diceButton' ><ul class='no-bullets diceData'><li>Roll Dices</li></ul></a></td>"
+        } else
+            for (pit = 0; pit < pitSize; pit++) {
+
+                pitType = "";
+
+
+                if (boardRow == 0) {
+                    pitId = Math.abs(pit - 12) + (boardRow * pitSize) - 1;
+                    pitType = "pitTop";
+                } else {
+                    pitId = pit + pitSize;
+                    pitType = "pitBottom";
+                }
+
+                rowData = rowData + '<td class="pit ' + pitType + '"><ul id="' + (pitId) + '" class="no-bullets"><li>' + (pitId) + '</li></ul>' +
+                    '</td>';
+                if (pitId === 6 || pitId == 17) {
+                    rowData = rowData + '<td class="boardCenter">&nbsp;</td>';
+                }
+
             }
-            // if (player === 'ONE' && pit === 5) {
-            //     rowData = rowData + treasure.replace("{PLAYERNAME}", "TWO");
-            // }
-        }
         rowData = rowData + "</tr>";
         $('.board table> tbody:last').append($(rowData));
     });
